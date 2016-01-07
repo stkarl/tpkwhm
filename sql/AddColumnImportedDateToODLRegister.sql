1dee6bec9a9603e94db3d87a0456dfe6cc6f47fd
saintkarl <github.karl@gmail.com>
2016-01-07 21:07:35 +0700

    Clean

--- sql/AddColumnImportedDateToODLRegister.sql
+++
@@ -1,3 +0,0 @@
-Alter table outletlocationregister add column `ImportedDate` DATETIME DEFAULT NULL;
-
-Alter table outletdistributionregistered add column `ImportedDate` DATETIME DEFAULT NULL;
--- sql/EditTableSubFullRangeBrand.sql
+++
@@ -1,5 +0,0 @@
---
--- Add Column DisplayOrder for table `subfullrangebrand`
---
-ALTER TABLE `subfullrangebrand` ADD `DisplayOrder` int(11) DEFAULT NULL
-
